c57ffc1d0099d6de09aa7f4affdb3758e67cd186
Matthew Blode <m.blode@gmail.com>
2017-03-01 12:03:37 +1100

    general

--- craft/config/general.php
+++ craft/config/general.php
@@ -1,25 +1,20 @@
-<?php
 
+<?php
 /**
  * General Configuration
  *
  * All of your system's general configuration settings go in here.
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
-
+// $_ENV constants are loaded by craft-multi-environment from .env.php via public/index.php
 return array(
-
     // All environments
     '*' => array(
-        'autoLoginAfterAccountActivation' => true,
         'omitScriptNameInUrls' => true,
         'usePathInfo' => true,
         'cacheDuration' => false,
         'useEmailAsUsername' => true,
         'generateTransformsBeforePageLoad' => true,
-        'rememberedUserSessionDuration' => 'P1Y',
-        'userSessionDuration' => 'P1D',
-        'maxUploadFileSize' => 104857600,
         'siteUrl' => getenv('CRAFTENV_SITE_URL'),
         'craftEnv' => CRAFT_ENVIRONMENT,
         // Set the environmental variables
@@ -45,6 +40,5 @@ return array(
         'devMode' => true,
         'enableTemplateCaching' => false,
         'allowAutoUpdates' => true,
-    )
-
+    ),
 );
