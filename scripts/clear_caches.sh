e2ee104aa176725790bdc07ff340958044e41834
Matthew Blode <m.blode@gmail.com>
2017-03-21 09:30:06 +1100

    craft install

--- scripts/clear_caches.sh
+++ scripts/clear_caches.sh
@@ -7,37 +7,51 @@
 # @author    nystudio107
 # @copyright Copyright (c) 2017 nystudio107
 # @link      https://nystudio107.com/
-# @package   craft-permissions
-# @since     1.0.2
+# @package   craft-scripts
+# @since     1.1.0
 # @license   MIT
+
+# Get the directory of the currently executing script
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+# Include files
+INCLUDE_FILES=(
+            "common/defaults.sh"
+            ".env.sh"
+            "common/common_env.sh"
+            "common/common_db.sh"
+            )
+for INCLUDE_FILE in "${INCLUDE_FILES[@]}"
+do
+    if [ -f "${DIR}/${INCLUDE_FILE}" ]
+    then
+        source "${DIR}/${INCLUDE_FILE}"
+    else
+        echo 'File "${DIR}/${INCLUDE_FILE}" is missing, aborting.'
+        exit 1
+    fi
+done
 
 # The permissions for files & directories that need to be writeable
 WRITEABLE_DIR_PERMS=775  # `-rwxrwxr-x`
 
-# Local directories relative to LOCAL_ROOT_PATH that should be removed to clear the cache
+# Local directories relative to LOCAL_CRAFT_FILES_PATH that should be removed to clear the cache
 CRAFT_CACHE_DIRS=(
-                "craft/storage/runtime/cache"
-                "craft/storage/runtime/compiled_templates"
-                "craft/storage/runtime/state"
+                "runtime/cache"
+                "runtime/compiled_templates"
+                "runtime/state"
                 )
 
 # Craft Database tables to be emptied to clear the cache
 CRAFT_CACHE_TABLES=(
+                "cache"
                 "templatecaches"
                 )
-
-# Make sure the `.env.sh` exists
-if [[ ! -f ".env.sh" ]] ; then
-    echo 'File ".env.sh" is missing, aborting.'
-    exit
-fi
-
-source ".env.sh"
 
 # Delete the cache dirs
 for DIR in ${CRAFT_CACHE_DIRS[@]}
     do
-        FULLPATH="${LOCAL_ROOT_PATH}${DIR}"
+        FULLPATH="${LOCAL_CRAFT_FILES_PATH}${DIR}"
         if [ -d "${FULLPATH}" ]
         then
             echo "Removing cache dir ${FULLPATH}"
@@ -54,7 +68,10 @@ for TABLE in ${CRAFT_CACHE_TABLES[@]}
     do
         FULLTABLE=${GLOBAL_DB_TABLE_PREFIX}${TABLE}
         echo "Emptying cache table $FULLTABLE"
-        $LOCAL_MYSQL_CMD --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASSWORD} ${LOCAL_DB_NAME} -e \
+        $LOCAL_MYSQL_CMD $LOCAL_DB_CREDS -e \
             "DELETE FROM $FULLTABLE"
     done
 echo "*** Caches cleared"
+
+# Normal exit
+exit 0
--- scripts/set_perms.sh
+++ scripts/set_perms.sh
@@ -7,9 +7,29 @@
 # @author    nystudio107
 # @copyright Copyright (c) 2017 nystudio107
 # @link      https://nystudio107.com/
-# @package   craft-permissions
-# @since     1.0.2
+# @package   craft-scripts
+# @since     1.1.0
 # @license   MIT
+
+# Get the directory of the currently executing script
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+# Include files
+INCLUDE_FILES=(
+            "common/defaults.sh"
+            ".env.sh"
+            "common/common_env.sh"
+            )
+for INCLUDE_FILE in "${INCLUDE_FILES[@]}"
+do
+    if [ -f "${DIR}/${INCLUDE_FILE}" ]
+    then
+        source "${DIR}/${INCLUDE_FILE}"
+    else
+        echo 'File "${DIR}/${INCLUDE_FILE}" is missing, aborting.'
+        exit 1
+    fi
+done
 
 # The permissions for all files & directories in the Craft CMS install
 GLOBAL_DIR_PERMS=755     # `-rwxr-xr-x`
@@ -19,14 +39,7 @@ GLOBAL_FILE_PERMS=644    # `-rw-r--r--`
 WRITEABLE_DIR_PERMS=775  # `-rwxrwxr-x`
 WRITEABLE_FILE_PERMS=664 # `-rw-rw-r--`
 
-# Make sure the `.env.sh` exists
-if [[ ! -f ".env.sh" ]] ; then
-    echo 'File ".env.sh" is missing, aborting.'
-    exit
-fi
-
-source ".env.sh"
-
+# Set project permissions
 echo "Setting base permissions for the project ${LOCAL_ROOT_PATH}"
 chown -R ${LOCAL_CHOWN_USER}:${LOCAL_CHOWN_GROUP} "${LOCAL_ROOT_PATH}"
 chmod -R ${GLOBAL_DIR_PERMS} "${LOCAL_ROOT_PATH}"
@@ -46,3 +59,6 @@ for DIR in ${LOCAL_WRITEABLE_DIRS[@]}
             chmod -R $WRITEABLE_DIR_PERMS "${FULLPATH}"
         fi
     done
+
+# Normal exit
+exit 0
